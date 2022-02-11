80c71a18f7c99562acb929d503c9bdaddc3dcf13
ManaRice <h19douli@gmail.com>
2022-02-11 18:02:23 +0100

    [Sudoku] Changed var to let because I think they make more sence

--- sudoku/index.js
+++ sudoku/index.js
@@ -69,7 +69,7 @@ class Board {
     // Used to run the solve() method.
     // This method may leave some solved cells in the board.
     clone() {
-        var copy = new Board(this.difficulty);
+        let copy = new Board(this.difficulty);
         copy.numberList = [...this.numberList];
         copy.cells = [...this.cells];
         return copy;
@@ -77,7 +77,7 @@ class Board {
 
     // Inatiate the board with zeros.
     zeroCells() {
-        for (var i = 0; i < this.width * this.height; i++) {
+        for (let i = 0; i < this.width * this.height; i++) {
             this.cells.push(new Cell(0));
         }
     }
@@ -97,8 +97,8 @@ class Board {
     // Used to generate random boards
     // Stolen from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     shuffleNumberlist(){
-        for (var i = this.numberList.length - 1; i > 0; i--) {
-            var rand = Math.floor(Math.random() * (i + 1));
+        for (let i = this.numberList.length - 1; i > 0; i--) {
+            let rand = Math.floor(Math.random() * (i + 1));
             [this.numberList[i], this.numberList[rand]] = [this.numberList[rand], this.numberList[i]]
         }
     }
@@ -107,9 +107,9 @@ class Board {
     // Exept for multiple zeros.
     // Used for checking if row, column or square is valid
     uniqueArray(arr) {
-        var temp = [];
-        for (var i = 0; i < arr.length; i++) {
-            for (var j = 0; j < temp.length; j++) {
+        let temp = [];
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < temp.length; j++) {
                 if (arr[i] == 0 || temp[i] == 0)
                     continue;
                 if (arr[i] == temp[j])
@@ -121,7 +121,7 @@ class Board {
     }
 
     boardContainsZero() {
-        for (var i = 0; i < this.cells.length; i++)
+        for (let i = 0; i < this.cells.length; i++)
             if (this.cells[i].val == 0)
                 return true;
         return false;
@@ -131,9 +131,9 @@ class Board {
     // Returns an array containing the column the cell of "index"
     // belongs to
     getCol(index) {
-        var col = [];
-        var coli = index % 9;
-        for (var i = coli; i < this.cells.length; i += 9) {
+        let col = [];
+        let coli = index % 9;
+        for (let i = coli; i < this.cells.length; i += 9) {
             col.push(this.cells[i].val);
         }
         return col;
@@ -141,9 +141,9 @@ class Board {
     // Returns an array containing the row the cell of "index"
     // belongs to
     getRow(index) {
-        var row = [];
-        var rowi = index - index % 9;
-        for (var i = rowi; i < rowi + 9; i++) {
+        let row = [];
+        let rowi = index - index % 9;
+        for (let i = rowi; i < rowi + 9; i++) {
             row.push(this.cells[i].val);
         }
         return row;
@@ -152,9 +152,9 @@ class Board {
     // Returns an array containing the square the cell of "index"
     // belongs to
     getSqr(index) {
-        var sqr = [];
-        var sqri = ((index % 9) - (index % 3)) + Math.floor(Math.floor(index / 9) / 3) * 9 * 3;
-        for (var i = 0; i < 9; i++){
+        let sqr = [];
+        let sqri = ((index % 9) - (index % 3)) + Math.floor(Math.floor(index / 9) / 3) * 9 * 3;
+        for (let i = 0; i < 9; i++){
             sqr.push(this.cells[sqri + (i % 3) + ( Math.floor(i / 3) * 9)].val)
         }
         return sqr;
@@ -165,14 +165,14 @@ class Board {
         if (this.boardContainsZero())
             return false;
 
-        for (var i = 0; i < 9; i++) {
-            var row = [];
-            var col = [];
-            var sqr = [];
-            for (var j = 0; j < 9; j++) {
-                var rowi = j + i * 9;
-                var coli = i + j * 9;
-                var sqri = ((j % 3) + ((i % 3) * 3)) + (Math.floor(j / 3) + (Math.floor(i / 3) * 3)) * 9;
+        for (let i = 0; i < 9; i++) {
+            let row = [];
+            let col = [];
+            let sqr = [];
+            for (let j = 0; j < 9; j++) {
+                let rowi = j + i * 9;
+                let coli = i + j * 9;
+                let sqri = ((j % 3) + ((i % 3) * 3)) + (Math.floor(j / 3) + (Math.floor(i / 3) * 3)) * 9;
                 row.push(this.cells[rowi].val);
                 col.push(this.cells[coli].val);
                 sqr.push(this.cells[sqri].val);
@@ -188,13 +188,14 @@ class Board {
     // Will return max 2, because if the number of solutions are greater
     // than 1 it is not a valid sudoku board so it is irelevant calculations
     solve(counter) {
-        for (var i = 0; i < this.cells.length; i++) {
+        let i;
+        for (i = 0; i < this.cells.length; i++) {
             if (this.cells[i].val == 0) {
-                for (var j = 1; j < 10; j++) {
+                for (let j = 1; j < 10; j++) {
                     this.cells[i] = new Cell(j);
-                    var row = this.getRow(i);
-                    var col = this.getCol(i);
-                    var sqr = this.getSqr(i);
+                    let row = this.getRow(i);
+                    let col = this.getCol(i);
+                    let sqr = this.getSqr(i);
 
                     if (this.uniqueArray(row) && this.uniqueArray(col) && this.uniqueArray(sqr)) {
                         if (this.check()) {
@@ -216,16 +217,16 @@ class Board {
 
     // Generates a fully solved valid sudoku board
     generateFullBoard() {
-        var i;
+        let i;
         for (i = 0; i < this.cells.length; i++) {
             if (this.cells[i].val == 0) {
                 this.shuffleNumberlist();
 
-                for (var j = 0; j < this.numberList.length; j++) {
+                for (let j = 0; j < this.numberList.length; j++) {
                     this.cells[i] = new Cell(this.numberList[j]);
-                    var row = this.getRow(i);
-                    var col = this.getCol(i);
-                    var sqr = this.getSqr(i);
+                    let row = this.getRow(i);
+                    let col = this.getCol(i);
+                    let sqr = this.getSqr(i);
 
                     if (this.uniqueArray(row) && this.uniqueArray(col) && this.uniqueArray(sqr)){
                         if (this.check())
@@ -244,7 +245,7 @@ class Board {
 
     // Returns if the current board is valid e.i has exactly 1 solution
     isValid() {
-        var copy = this.clone();
+        let copy = this.clone();
         return copy.solve(0) < 2;
     }
 
@@ -252,10 +253,10 @@ class Board {
     generateNewBoard() {
         this.zeroCells();
         this.generateFullBoard();
-        var index;
-        var cellVal;
-        var copy;
-        for (var i = 0; i < this.difficulty; i++) {
+        let index;
+        let cellVal;
+        let copy;
+        for (let i = 0; i < this.difficulty; i++) {
             do {
                 do {
                     index = Math.floor(Math.random() * 80);
@@ -287,13 +288,13 @@ class Board {
  *
  */
 
-var board = new Board(HARD);
+let board = new Board(HARD);
 board.generateNewBoard();
 updateBoard();
 
 // Updates the visual board, and checks if it is solved
 function updateBoard(){
-    for (var i = 0; i < board.cells.length; i++) {
+    for (let i = 0; i < board.cells.length; i++) {
         let cell = board.cell(i);
         let div = document.getElementById(i.toString());
         if (!div) {
@@ -315,7 +316,7 @@ function selectNumber(id) {
     if (!sel.classList.contains("active")){
         sel.classList.add("active");
     }
-    for (var i = 0; i < board.cells.length; i++) {
+    for (let i = 0; i < board.cells.length; i++) {
         document.getElementById(i.toString()).classList.remove("selected");
     }
     document.getElementById(id.toString()).classList.add("selected");
@@ -327,8 +328,8 @@ function changeNumber(number) {
     let div = document.getElementById("sel");
     if (!div.classList.contains("active")) return;
 
-    var index_holder =  document.getElementById('index-holder');
-    var index = index_holder.innerHTML;
+    let index_holder =  document.getElementById('index-holder');
+    let index = index_holder.innerHTML;
 
     board.cells[index].val = number;
     index_holder.innerHTML = index.toString();
